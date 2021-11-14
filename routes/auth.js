d22d643efc8ce00980f8615192e6cdafabd7d4a8
Mellang <dlwpgh23@naver.com>
2021-11-15 00:00:56 +0900

    body-parser적용

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
-import express from "express";
+// import express from "express";
+const { default: axios } = require("axios");
+const express = require("express");
 const router = express.Router();
 
-const address = "http://localhost:5000/";
-
 router.post("/signin", (req, res) => {
-    
+  const {
+    body: { username, password },
+  } = req;
+  console.log("리퀘에용", username, password);
+  res.send({ username: "헬로!!", createdAt: "123" });
 });
 
-export default router;
-// module.exports = router;
+// export default router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-import express from "express";
-import cors from "cors";
+// import express from "express";
+// import cors from "cors";
+const express = require("express");
+const cors = require("cors");
+const signIn = require("./routes/auth");
 
 const app = express();
 const route = express.Router();
@@ -9,6 +12,11 @@ let corsOption = {
   optionsSuccessStatus: 200,
 };
 
-app.use("/auth", cors(corsOption));
+//Express 4.x버전에는 body-parser모듈 내장
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(5000);
+app.use("/auth", cors(corsOption), signIn);
+
+const port = 5000; //노드 서버가 사용할 포트
+app.listen(port, () => console.log(`Listening on port ${port}`));
