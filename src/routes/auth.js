3e42d92d3e81dc0ecfc112206820663f69287eb1
Mellang <dlwpgh23@naver.com>
2021-11-18 22:10:52 +0900

    bcrypt password and save user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { signUpUser } from "../models/SignupUser.js";
-import promisify from "util";
+const userModel = signUpUser();
 // const { default: axios } = require("axios");
 // const express = require("express");
 // const mongoose = require("mongoose");
@@ -13,9 +13,29 @@ userRouter.post("/signup", (req, res) => {
     body: { username, password },
   } = req;
   const email = username + "test@test.com";
+  const currDate = new Date();
   console.log("사인업이에용", username, password);
-  signUpUser(username, email, password);
-  res.send({ username: "헬로!!", createdAt: "123" });
+  const User = new userModel({
+    name: username,
+    email,
+    password,
+    createdAt: currDate,
+  });
+  userModel.findOne({ email: email }).then((response) => {
+    if (response != null) {
+      console.log("이미 있는 이메일 입니다.");
+      res.send({ status: false, message: "invailed email!" });
+      throw Error("이미있습니다!");
+    }
+    try {
+      User.save();
+      console.log("저장완료");
+      res.send({ status: true, message: "success" });
+    } catch (e) {
+      console.log("저장에러", e);
+      res.send({ status: false, message: "save error" });
+    }
+  });
 });
 
 userRouter.post("/signin", (req, res) => {
@@ -29,7 +49,7 @@ userRouter.post("/signin", (req, res) => {
 
   // kitty.save().then((r) => console.log("meow", r));
 
-  Cat.findOne({ name: "멀바슈방" }).then((r) => console.log("미야옹", r));
+  Cat.findOne({ name: "땅땅이" }).then((r) => console.log("미야옹", r));
   res.send({ username: "헬로!!", createdAt: "1234" });
 });
 
