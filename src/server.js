929d80d6cc543d14dfcbf6233f3cf5e33fcf8ca7
Mellang <dlwpgh23@naver.com>
2021-12-08 18:24:56 +0900

    cors

--- src/server.js
+++ src/server.js
@@ -32,13 +32,16 @@ let corsOption = {
   origin: ["http://localhost:3000", `https://project-dm.netlify.app/`],
   optionsSuccessStatus: 200,
 };
+app.use(cors());
 
 //Express 4.x버전부터 body-parser모듈 내장
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/auth", cors(corsOption), userRouter);
-app.use("/todo", cors(corsOption), todoRouter);
+app.use("/auth", userRouter);
+app.use("/todo", todoRouter);
+// app.use("/auth", cors(corsOption), userRouter);
+// app.use("/todo", cors(corsOption), todoRouter);
 
 const port = process.env.PORT || 5000; //노드 서버가 사용할 포트
 app.listen(port, () => console.log(`Listening on port ${port}`));
